ed5cff33359a5f00792e5d5de7bd92e7d896afd9
Max Hobday <mhobday@unomaha.edu>
2021-05-01 16:25:31 -0500

    sql for login works

--- Max/home2.js
+++ Max/home2.js
@@ -6,7 +6,7 @@ let server = require('http').createServer(app);
 
 let mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var startSQL = mysql.createConnection({
     host: 'ec2-18-222-6-224.us-east-2.compute.amazonaws.com',
     user: 'babyboiremote',
     password: 'babyboi',
@@ -96,29 +96,29 @@ io.sockets.on('connection', function (socket) {
 
   socket.on('login', function(username, password) {
     let playerId;
-    let passed = false; //passed will be the value for if they log in
     console.log(username);
     console.log(password);
     //put database check code here
-    connection.connect();
-    connection.query('SELECT ' + username + ' FROM users AS data', function(error, results, fields) {
-    if (error) throw error;
-    else if(password = results[0].password) passed = true;
-    });
-    connection.end();
-    if(passed){
+    startSQL.query('SELECT * FROM users AS data WHERE username = \'' + username + '\'', function(error, results, fields) {
+    if (error)
+    {
+      throw error;
+    } 
+    else if(password == results[0].password)
+    {
+      console.log("passed is true");
       playerId = Math.random();
       SOCKET_LIST[playerId] = socket;
       SOCKETID_LIST[socket.id] = playerId;
       USERNAME_LIST[playerId] = username;
       socket.emit("newPlayer", playerId);
-    }
-    else{
+    } 
+    else 
+    {
       socket.emit('bad_login');
     }
+    })
   })
-
-  connection.end();
 
   socket.on('turnCheck', function (clickedBy) {
     console.log(clickedBy);
