80eaeb4b7eafee6eaa527867b8c7853721ca6aa7
gdrew1 <78456408+gdrew1@users.noreply.github.com>
2021-05-03 19:01:27 -0500

    web pages have earned their names

--- Selenium/selenium.js
+++ Selenium/selenium.js
@@ -8,7 +8,7 @@ chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build());
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://www.google.com/ncr');
+    await driver.get('http://ec2-3-140-1-42.us-east-2.compute.amazonaws.com:4141');
     await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
     await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
